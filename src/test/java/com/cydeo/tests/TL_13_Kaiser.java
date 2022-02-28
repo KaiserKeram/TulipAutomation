ca1b4e84d74b2743b9484f8441c810aa5e02c159
kaiser <ulug0829@gmail.com>
2022-02-28 01:56:53 -0500

    TL-13_Kaiser

--- src/test/java/com/cydeo/tests/TL_13_Kaiser.java
+++ src/test/java/com/cydeo/tests/TL_13_Kaiser.java
@@ -53,7 +53,7 @@ public class TL_13_Kaiser {
         AppreciationBtn.click();
 
         // Enter Appreciation words
-        String AppreciationWords = "Thank you so much!";
+        String AppreciationWords = "Thank you!";
         driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']")));
         driver.findElement(By.xpath("//body")).sendKeys(Keys.CLEAR + AppreciationWords);
 
