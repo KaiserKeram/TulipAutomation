d6ec6ba4512e28ac19c6b69666381deb4e9e198e
alimu0223 <alimu0223@outlook.com>
2022-02-28 01:35:34 -0500

    User Story 08 updated, and done

--- src/test/java/com/cydeo/tests/US08_Ali.java
+++ src/test/java/com/cydeo/tests/US08_Ali.java
@@ -8,62 +8,71 @@ Notifications
 Settings
 Active Stream
 
-https://login2.nextbasecrm.com/
-hr31@cydeo.com
-UserUser
  */
-        import io.github.bonigarcia.wdm.WebDriverManager;
-        import org.openqa.selenium.By;
-        import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.WebElement;
-        import org.openqa.selenium.chrome.ChromeDriver;
-        import org.testng.Assert;
-        import org.testng.annotations.BeforeClass;
-        import org.testng.annotations.Test;
 
-        import java.util.ArrayList;
-        import java.util.Arrays;
-        import java.util.Collections;
-        import java.util.List;
-        import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class US08_Ali {
     public static WebDriver driver;
+    public static Properties properties = new Properties();
 
-    @BeforeClass
-    public static void driverSetup(){
+    {
+        try {
+            FileInputStream file = new FileInputStream("us08properties.properties");
+            properties.load(file);
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String keyword) {
+        return properties.getProperty(keyword);
+    }
+
+    @BeforeMethod
+    public static void driverSetup() throws Exception {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+    @AfterMethod
+    public static void  closeDriver(){
+        driver.close();
+    }
+
 
     @Test
-    public static void clickAndVarifyChatAndCallsModule(){
-        driver.get("https://login2.nextbasecrm.com/");
-        driver.findElement(By.name("USER_LOGIN")).sendKeys("hr31@cydeo.com");
-        driver.findElement(By.xpath("//div[@class='login-item']/input[@type='password']")).sendKeys("UserUser");
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.findElement(By.xpath("//a[@href='/online/']")).click();
-        try {
+    public static void clickAndVarifyChatAndCallsModule() throws InterruptedException{
+        driver.get(getProperty("url"));
+        driver.findElement(By.name("USER_LOGIN")).sendKeys(getProperty("username1"));
+        driver.findElement(By.xpath("//div[@class='login-item']/input[@type='password']")).sendKeys(getProperty("password"));
             Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+            Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@href='/online/']")).click();
+            Thread.sleep(2000);
+
         List<WebElement> elements = driver.findElements(By.xpath("//*[contains(@class,'bx-desktop-tab')]"));
 
-        List<String> expected = new ArrayList<>(Arrays.asList("Message","Notifications","Settings","Active Stream"));
+        List<String> expected = new ArrayList<>(Arrays.asList("Message", "Notifications", "Settings", "Active Stream"));
         List<String> actual = new ArrayList<>();
 
         for (WebElement element : elements) {
@@ -72,7 +81,61 @@ public class US08_Ali {
         }
         actual.removeAll(Collections.singleton(""));
 
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
 
     }
+
+    @Test
+    public static void clickAndVarifyChatAndCallsModule2() throws InterruptedException{
+        driver.get(getProperty("url"));
+        driver.findElement(By.name("USER_LOGIN")).sendKeys(getProperty("username4"));
+        driver.findElement(By.xpath("//div[@class='login-item']/input[@type='password']")).sendKeys(getProperty("password"));
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@href='/online/']")).click();
+        Thread.sleep(2000);
+
+        List<WebElement> elements = driver.findElements(By.xpath("//*[contains(@class,'bx-desktop-tab')]"));
+
+        List<String> expected = new ArrayList<>(Arrays.asList("Message", "Notifications", "Settings", "Active Stream"));
+        List<String> actual = new ArrayList<>();
+
+        for (WebElement element : elements) {
+            String title = element.getAttribute("title");
+            actual.add(title);
+        }
+        actual.removeAll(Collections.singleton(""));
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public static void clickAndVarifyChatAndCallsModule3() throws InterruptedException{
+        driver.get(getProperty("url"));
+        driver.findElement(By.name("USER_LOGIN")).sendKeys(getProperty("username7"));
+        driver.findElement(By.xpath("//div[@class='login-item']/input[@type='password']")).sendKeys(getProperty("password"));
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@href='/online/']")).click();
+        Thread.sleep(2000);
+
+        List<WebElement> elements = driver.findElements(By.xpath("//*[contains(@class,'bx-desktop-tab')]"));
+
+        List<String> expected = new ArrayList<>(Arrays.asList("Message", "Notifications", "Settings", "Active Stream"));
+        List<String> actual = new ArrayList<>();
+
+        for (WebElement element : elements) {
+            String title = element.getAttribute("title");
+            actual.add(title);
+        }
+        actual.removeAll(Collections.singleton(""));
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+
 }
