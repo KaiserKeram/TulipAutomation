f0aaa3a74e9d2c1a7034ff839508d86731e7eea0
Elminur Ablimit <elminur226@gmail.com>
2022-03-01 00:28:44 -0800

    US02_TC02 is done

--- src/test/java/com/cydeo/tests/US02_TC02_elminur.java
+++ src/test/java/com/cydeo/tests/US02_TC02_elminur.java
@@ -1,4 +1,39 @@
 package com.cydeo.tests;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class US02_TC02_elminur {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://login2.nextbasecrm.com");
+    }
+
+    @Test
+    public void messageOnTheLoginPage() {
+        //verify title of page
+        String LoginPageActualTitle = driver.getTitle();
+        String LoginPageExpectedTile = "Authorization";
+        Assert.assertEquals(LoginPageActualTitle, LoginPageExpectedTile);
+
+        // “Remember me on this computer” displayed on the page
+        WebElement checkBoxLabel=driver.findElement(By.className("login-item-checkbox-label"));
+        String expectedLabel="Remember me on this computer";
+        String actualLabel=checkBoxLabel.getText();
+        Assert.assertEquals(expectedLabel,actualLabel);
+        System.out.println("checkBoxLabel.isDisplayed() = " + checkBoxLabel.isDisplayed());
+
+    }
 }
