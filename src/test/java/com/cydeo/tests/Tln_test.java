66967ed580348e978021d113558f12efc779a296
Jewel522 <hayatliq@hotmail.com>
2022-03-04 22:31:37 -0500

    'VOTE AGAIN displayed US8_VOTE done

--- src/test/java/com/cydeo/tests/Tln_test.java
+++ src/test/java/com/cydeo/tests/Tln_test.java
@@ -4,6 +4,7 @@ import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,7 +23,7 @@ public class Tln_test {
         //log in
         WebElement InputUserName = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
         InputUserName.sendKeys("hr31@cydeo.com"); // passed
-       // InputUserName.sendKeys("helpdesk1@cybertekschool.com"); //
+        // InputUserName.sendKeys("helpdesk1@cybertekschool.com"); //
         // InputUserName.sendKeys("marketing33@cybertekschool.com"); //
 
         WebElement InputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
@@ -37,20 +38,24 @@ public class Tln_test {
     public void locateVOTE() throws InterruptedException {
         //test case 2 test: task with no title;
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-          WebElement oneOption = driver.findElement(By.xpath("//*[@id=\"question1209\"]/table/tbody/tr[1]/td[1]/div/span/label[1]/span"));
+        WebElement oneOption = driver.findElement(By.xpath("//*[@id=\"question1211\"]/table/tbody/tr[1]/td[1]/div/span/label[1]/span"));
 
-          driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         oneOption.click();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         //
 
-     //   WebElement voteButton = driver.findElement(By.xpath("//*[@id=\"vote-vNG4pq1163\"]/form/div[2]/button[2]/text()"));
-        WebElement voteButton = driver.findElement(By.xpath("//button[@data-bx-vote-button='actVoting']" ));
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        WebElement voteButton = driver.findElement(By.xpath("//button[@data-bx-vote-button='actVoting']"));
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         voteButton.click();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-        //
+        //verify VOTE AGAIN is displayed
+
+        String expectedText = "VOTE AGAIN";
+        String actualText = driver.findElement(By.xpath("//*[@id=\"vote-mPlAdu1169\"]/form/div[2]/button[1]")).getText();
+
+        Assert.assertEquals(actualText, expectedText, "not match");
 
 
     }
